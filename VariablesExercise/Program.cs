21ec24eea8a7c720917ce8ca3bec64f3435e4cb2
Jessica Gonzales <jgonzales0688@gmail.com>
2022-11-18 22:05:33 -0600

    Variables added

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace VariablesExercise
+﻿using System.Globalization;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String myName = "Jessica Gonzales";
+            int currYear = 2022;
+            char midInital = 'M';
+            bool inCollege = true;
+            double yrsInCollege = 2.5;
+            decimal rndmDecimal = 3.2546m;
+            
+            Console.WriteLine("My name is " + myName);
+            Console.WriteLine("The year is " + currYear);
+                Console.WriteLine("My middle initial is " + midInital);
+            if (inCollege) {
+                Console.WriteLine("I have been in college for " + yrsInCollege + " years.");
+            }
+                Console.WriteLine("This is my random decimal " + rndmDecimal);
+
+
+                
         }
     }
 }
